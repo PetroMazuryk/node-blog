63b80cd68c10075a56641c4f06e115a0eeb9d1e6
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-05-02 00:15:31 +0300

    add validations auth

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import { validationResult } from "express-validator";
 
-// import mongoose from "mongoose";
-// const { DB_HOST, PORT } = process.env;
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => console.log("DB OK"))
-//   .catch((err) => console.log("DB err", err));
+import { registerValidation } from "./validations/auth.js";
 
 export const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World !");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World !");
+// });
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Petro Mazuryk",
-    },
-    "secret123"
-  );
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
 
   res.json({
     success: true,
-    token,
   });
 });
-
-// app.listen(4444, (err) => {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   console.log("Server OK");
-// });
